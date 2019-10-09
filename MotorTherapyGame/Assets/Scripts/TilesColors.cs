5a1c47c04805cfba30eb977453e73bcdb43dc185
Jose Acuña <jodaniel1409@gmail.com>
2019-10-09 01:56:47 -0600

    [ADD] Notes and NotesSpawner

---
+++ MotorTherapyGame/Assets/Scripts/TilesColors.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesColors : MonoBehaviour
+{
+    public enum Colors {Red, Green, Blue, Yellow};
+}
