8358ddaddf317271ad3c2966271963083e8e618f
Jose Acuña <jodaniel1409@gmail.com>
2019-10-09 22:12:05 -0600

    [ADD] Piano collision detection

---
+++ MotorTherapyGame/Assets/Scripts/PianoFlag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoFlag : MonoBehaviour
+{
+    private bool _noteCollision = false;
+
+    private void OnMouseDown()
+    {
+        if (_noteCollision) Hit();
+        else Miss();
+    }
+
+    private void OnCollisionEnter() => _noteCollision = true;
+
+    private void OnCollisionExit() => _noteCollision = false;
+
+    private void Hit()
+    {
+        Debug.Log("HIT");
+    }
+
+    private void Miss()
+    {
+        Debug.Log("MISS");
+    }
+}
