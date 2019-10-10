6b98914790764506a83fbf6718c3116ec4b19449
Jose Acuña <jodaniel1409@gmail.com>
2019-10-09 22:38:28 -0600

    [ADD] Audio effects on Piano hit or miss

--- MotorTherapyGame/Assets/Scripts/PianoFlag.cs
+++ MotorTherapyGame/Assets/Scripts/PianoFlag.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 
 public class PianoFlag : MonoBehaviour
 {
+    public AudioClip hitSound;
+    public AudioClip missSound;
     private bool _noteCollision = false;
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
 
     private void OnMouseDown()
     {
@@ -20,10 +28,14 @@ public class PianoFlag : MonoBehaviour
     private void Hit()
     {
         Debug.Log("HIT");
+        _audio.clip = hitSound;
+        _audio.Play();
     }
 
     private void Miss()
     {
         Debug.Log("MISS");
+        _audio.clip = missSound;
+        _audio.Play();
     }
 }
