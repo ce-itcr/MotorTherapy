e3f67703355f4b71f7ed57ba973e4f198a38c5aa
Jose Acuña <jodaniel1409@gmail.com>
2019-10-11 00:21:43 -0600

    [ADD] Score system

---
+++ MotorTherapyGame/Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Text scoreText;
+    public string tag = "";
+    private int score = 0;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        scoreText.text = $"{tag} : {score.ToString()}";
+    }
+
+    public void AddScore() => score += 1;
+}
