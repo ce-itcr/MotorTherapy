b54db85af1c7baf80ed780e837f44afc881c69c6
Iván <isolis@solavcr.com>
2019-10-16 18:58:53 -0600

    [UPDATE] Live mouse collision wip

---
+++ MotorTherapyGame/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    // Start is called before the first frame update
+    private void Start() {
+        
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        rigidbody.position = Input.mousePosition;
+    }
+}
---
+++ MotorTherapyGame/Assets/Scripts/Jump.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        rb.velocity = Vector3.zero;
+        if (Random.value > 0.5f)
+        {
+            rb.AddForce(1000 * Time.deltaTime, 20000 * Time.deltaTime, 0);
+        }
+        else
+            rb.AddForce(-1000 * Time.deltaTime, 20000 * Time.deltaTime, 0);
+    }
+
+    public void OnMouseDown() {
+        rb.velocity = Vector3.zero;
+        if (Random.value > 0.5f)
+        {
+            rb.AddForce(1000 * Time.deltaTime, 5000 * Time.deltaTime, 0);
+        }
+        else
+            rb.AddForce(-1000 * Time.deltaTime, 5000 * Time.deltaTime, 0);
+    }
+}
