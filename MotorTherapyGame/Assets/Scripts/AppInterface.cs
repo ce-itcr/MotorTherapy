c1467815fc0b56917e904edf7f42daa7081b78d3
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-10-29 18:23:32 -0600

    [UPDATE] Retrieve and Post Data

--- MotorTherapyGame/Assets/Scripts/AppInterface.cs
+++ MotorTherapyGame/Assets/Scripts/AppInterface.cs
@@ -13,7 +13,7 @@ public class AppInterface : MonoBehaviour
         // Start is called before the first frame update
     void Start()
     {
-        ShowGameSelectorCanvas();
+        ShowHomeCanvas();
     }
 
     // Update is called once per frame
