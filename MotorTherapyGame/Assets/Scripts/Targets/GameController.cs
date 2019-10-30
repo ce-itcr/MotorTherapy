9cba5e139e0bc8ad7a2a6a7445e568ef42134246
Jose Acuña <jodaniel1409@gmail.com>
2019-10-29 18:02:43 -0600

    [UPDATE] Targets working with client

---
+++ MotorTherapyGame/Assets/Scripts/Targets/GameController.cs
@@ -0,0 +1,51 @@
+﻿using Piano;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Targets
+{
+    public class GameController : MonoBehaviour
+    {
+        public GameObject timeController;
+        public Transform sphere;
+        private Transform _sphereIns;
+        private Client.Client _client;
+        private Game _game;
+
+        public void Spawn()
+        {
+            var message = JsonUtility.ToJson(new Game("targets", "ok"));
+            var response = _client.Message(message);
+            _game = Game.CreateFromJson(response);
+            var targets = _game.targets;
+            sphere = CreateSphere(targets.x, targets.y);
+            var timerScript = timeController.GetComponent<TimerCountdown>();
+            timerScript.sphereTime = targets.time;
+            timerScript.sphere = sphere;
+        }
+
+        public void End()
+        {
+            
+        }
+        
+        private void Awake()
+        {
+            _client = Client.Client.GetInstance();
+        }
+
+        private Transform CreateSphere(int x, int y)
+        {
+            if (_sphereIns != null) Destroy(_sphereIns.gameObject);
+            const float xOffset = 3.4f;
+            const float yOffset = 0.33f;
+            var xPoss = Mathf.Clamp(x, 0, 7) - xOffset;
+            var yPoss = Mathf.Clamp(y, 0, 4) - yOffset;
+            var poss = new Vector3(xPoss, yPoss,-3.05f);
+            _sphereIns = Instantiate(sphere, poss, sphere.transform.rotation);
+            _sphereIns.localScale = new Vector3(1.15f,1.15f,1.15f);
+            return _sphereIns;
+        }
+    }
+}
--- MotorTherapyGame/Assets/Scripts/Targets/TimerCountdown.cs
+++ MotorTherapyGame/Assets/Scripts/Targets/TimerCountdown.cs
@@ -1,45 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using Random = UnityEngine.Random;
 
-public class TimerCountdown : MonoBehaviour
+namespace Targets
 {
-    public Text textTime;
-    public float timeLeft;
-    public float sphereTime;
-    public Transform sphere;
-    public float scaleSize;
-    public float y;
-    public float x;
-    // Start is called before the first frame update
-    void Start()
+    public class TimerCountdown : MonoBehaviour
     {
-        timeLeft = 60.0f;
-        scaleSize = 1.15f;
-        sphereTime = 5.0f;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        scaleSize = sphere.localScale.x;
-        scaleSize -= (float) (0.95/sphereTime) * Time.deltaTime;
-        textTime.text = "Time: " + timeLeft.ToString("0");
-        sphere.localScale = new Vector3(scaleSize,scaleSize,scaleSize);
-        
-        if (scaleSize <= 0.15)
+        public GameObject gameController;
+        public Text textTime;
+        public float timeLeft;
+        public float sphereTime;
+        public Transform sphere;
+        public float scaleSize;
+        public float y;
+        public float x;
+        // Start is called before the first frame update
+        void Start()
         {
-            scaleSize = 1.15f;
-            sphere.localScale = new Vector3(scaleSize,scaleSize,scaleSize);
-            sphere.localPosition = new Vector3(Random.Range(-3.6f, 3.6f),Random.Range(0.68f,4.35f),-3.05f);
-        }
-        if(timeLeft <= 0){
             timeLeft = 60.0f;
+            scaleSize = 1.15f;
+            sphereTime = 5.0f;
+            gameController.SendMessage("Spawn");
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+            timeLeft -= Time.deltaTime;
+            scaleSize = sphere.localScale.x;
+            scaleSize -= (float) (0.95/sphereTime) * Time.deltaTime;
+            textTime.text = "Time: " + timeLeft.ToString("0");
+            sphere.localScale = new Vector3(scaleSize,scaleSize,scaleSize);
         
+            if (scaleSize <= 0.15)
+            {
+                scaleSize = 1.15f;
+                gameController.SendMessage("Spawn");
+            }
+            if(timeLeft <= 0){
+                gameController.SendMessage("End");
+            }
+        }
     }
 }
