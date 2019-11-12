9ee8815ccded726747686359f9e9bfc5576bae67
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-11-12 13:15:31 -0600

    [UPDATE] Basic Info Fragment

--- Patient-Stats/app/src/main/java/com/ceitcr/motortherapy/ui/info/InfoFragment.java
+++ Patient-Stats/app/src/main/java/com/ceitcr/motortherapy/ui/info/InfoFragment.java
@@ -23,13 +23,6 @@ public class InfoFragment extends Fragment {
         infoViewModel =
                 ViewModelProviders.of(this).get(InfoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_info, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        infoViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
