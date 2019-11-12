b008e0d8e8aadb6f2f3edca7088e2fd39e4a5daa
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-11-12 13:40:08 -0600

    [ADD] Initial Patient Info

--- Patient-Stats/app/src/main/java/com/ceitcr/motortherapy/ui/home/HomeFragment.java
+++ Patient-Stats/app/src/main/java/com/ceitcr/motortherapy/ui/home/HomeFragment.java
@@ -47,34 +47,24 @@ public class HomeFragment extends Fragment {
     private void initImageBitmaps(View root){
         Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
 
-        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        mNames.add("Havasu Falls");
-
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
-
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
-
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
-
-
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Mahahual");
-
-        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-        mNames.add("Frozen Lake");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacient");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacient");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacient");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacient");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacients");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacients");
+        mImageUrls.add("https://www.it.iitb.ac.in/frg/wiki/images/thumb/d/dc/User.jpg/960px-User.jpg");
+        mNames.add("TestPacients");
 
 
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("White Sands Desert");
 
-        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-        mNames.add("Austrailia");
 
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Washington");
 
         initRecyclerView(root);
     }
