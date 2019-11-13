d07aadaa85a74364fd96b5c250b021b7e64b904a
Jose Acuña <jodaniel1409@gmail.com>
2019-11-12 23:45:55 -0600

    [UPDATE] CobWeb Matrix placed on Ground

--- MotorTherapyGame/Assets/Scripts/CobWeb/CobWeb.cs
+++ MotorTherapyGame/Assets/Scripts/CobWeb/CobWeb.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,8 +10,10 @@ namespace CobWeb
         private Client.Client _client;
         private Game _game;
         public GameObject target;
-        public int height;
-        public int width;
+        public GameObject ground;
+        private float _height;
+        private float _width;
+        private List<List<GameObject>> _matrix;
 
 
         // Returns to the previous Scene
@@ -26,22 +29,52 @@ namespace CobWeb
 
         private void Start()
         {
+            // Connects to Server
             var message = JsonUtility.ToJson(new Game("cobWeb", "ok"));
             var response = _client.Message(message);
             _game = Game.CreateFromJson(response);
+
+            // Scale of the Ground and Instantiate Targets
+            var scale = ground.GetComponent<MeshRenderer>().bounds.size;
+            _height = scale.z;
+            _width = scale.x;
+            
+            InstantiatedTargets();
+            CreatePaths();
         }
 
-        public void instatiateTargets()
+        private void InstantiatedTargets()
         {
+            var rows = 5;
+            var columns = 5;
+            var xOffset = _width / (rows + 1);
+            var zOffset = _height / (columns + 1);
+            var xInitial = _width / 2 + xOffset / 4;
+            var zInitial = _height / 2;
+            
+            for (var j = 0; j < rows; ++j)
             {
-                for (int y = 0; y < height; ++y)
+                var z = (j + 1) * zOffset - zInitial;
+                var row = new List<GameObject>();
+                for (var i = 0; i < columns; ++i)
                 {
-                    for (int x = 0; x < width; ++x)
-                    {
-                        Instantiate(target, new Vector3(x, y, 0), Quaternion.identity);
-                    }
+                    // Creates a gap between rows
+                    var odd = 0f;
+                    if (j % 2 != 0) odd = xOffset / 2;
+                    
+                    // Creates the Target
+                    var x = (i + 1) * xOffset - xInitial + odd;
+                    var obj = Instantiate(target, new Vector3(x,0, z), Quaternion.identity);
+                    row.Add(obj);
                 }
+                _matrix.Add(row);
             }
         }
+
+        private void CreatePaths()
+        {
+            // Reads the matrix nad generates the connections
+        }
+        
     }
 }
