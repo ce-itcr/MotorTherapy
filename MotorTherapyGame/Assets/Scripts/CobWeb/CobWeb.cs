43f2a78b8778274e3779596b9db635d216906eef
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-11-12 20:26:27 -0600

    [ADD] Instantiate Target Goals in Plane

--- MotorTherapyGame/Assets/Scripts/CobWeb/CobWeb.cs
+++ MotorTherapyGame/Assets/Scripts/CobWeb/CobWeb.cs
@@ -8,13 +8,17 @@ namespace CobWeb
     {
         private Client.Client _client;
         private Game _game;
-        
+        public GameObject target;
+        public int height;
+        public int width;
+
+
         // Returns to the previous Scene
         public void Back()
         {
             SceneManager.LoadScene("AppInterface");
         }
-        
+
         private void Awake()
         {
             _client = Client.Client.GetInstance();
@@ -25,6 +29,19 @@ namespace CobWeb
             var message = JsonUtility.ToJson(new Game("cobWeb", "ok"));
             var response = _client.Message(message);
             _game = Game.CreateFromJson(response);
+        }
+
+        public void instatiateTargets()
+        {
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    for (int x = 0; x < width; ++x)
+                    {
+                        Instantiate(target, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+            }
         }
     }
 }
